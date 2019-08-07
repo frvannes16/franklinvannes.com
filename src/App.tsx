0c85d99733c068eb5dd12f6ff37575b7e0ec3906
Franklin van Nes <fvannes@tripadvisor.com>
2019-08-07 19:10:33 -0400

    Dockerfile and Page.tsx:

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import BlockLogo from './components/block-logo/BlockLogo';
 import Menu from './components/menu/Menu';
+import Page from './components/page/Page';
 import styles from './App.module.scss';
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className={styles.page}>
       <BlockLogo>FRANKLIN</BlockLogo>
       <div className={styles.menuWrapper}>
         <Menu />
       </div>
+      <Page title="Home"><p>Welcome to franklinvannes.com</p></Page>
     </div>
   );
 };
